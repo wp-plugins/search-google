2178f736f89a6f70978d255f7c2a2d8ec79904f8
webvitaly <webvitaly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-15 12:58:12 +0000

    1.3 commit;
    git-svn-id: https://plugins.svn.wordpress.org/search-google/tags/1.3@490205 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Search by Google ===
+Contributors: webvitaly
+Plugin URI: http://web-profile.com.ua/wordpress/plugins/search-google/
+Tags: google, search, widget
+Author URI: http://web-profile.com.ua/wordpress/
+Requires at least: 3.0
+Tested up to: 3.3.1
+Stable tag: 1.3
+
+"Search by Google" plugin adds Google search form widget.
+
+== Description ==
+
+"Search by Google" plugin adds Google search form widget. It helps user to search on site using Google.
+
+= Widget options: =
+* "Title" (title of the widget);
+* "Submit button text" (if left blank than "Google search" text will be used);
+* "Search on site" (if left blank than Google will search on current site);
+
+[Search by Google plugin page](http://web-profile.com.ua/wordpress/plugins/search-google/)
+
+[CMS WordPress](http://web-profile.com.ua/wordpress/)
+
+== Screenshots ==
+
+1. "Search by Google" widget;
+2. "Search by Google" widget on site;
+2. Google search page after form submit;
+
+== Changelog ==
+
+= 1.3 =
+* Improved scripts;
+
+= 1.2 =
+* Improved scripts and styles include;
+
+= 1.1 =
+* Improved input focus behaviour;
+
+= 1.0.0 =
+* Initial release;
+
+== Installation ==
+
+1. Install and activate the plugin on the Plugins page;
+2. Add "Search by Google" widget to your sidebar and customize it;
---
+++ search-google.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Plugin Name: Search by Google
+Plugin URI: http://web-profile.com.ua/wordpress/plugins/search-google/
+Description: Adds Google search form widget.
+Version: 1.3
+Author: webvitaly
+Author Email: webvitaly(at)gmail.com
+Author URI: http://web-profile.com.ua/wordpress/
+
+Future features:
+- add support of multiple widgets with search-by-google form;
+*/
+
+class WP_Widget_Search_Google extends WP_Widget {
+
+	function WP_Widget_Search_Google() {
+		$widget_ops = array('classname' => 'widget_search_google', 'description' => __( 'Search by Google widget' ) );
+		$this->WP_Widget('search_google', __('Search by Google'), $widget_ops);
+	}
+	
+	function widget( $args, $instance ) {
+		extract($args);
+		$title = apply_filters('widget_title', $instance['title']);
+		$submit_text = empty($instance['submit_text']) ? __('Search Google') : $instance['submit_text'];
+		$site_search = empty($instance['site_search']) ? get_bloginfo('home') : $instance['site_search'];
+		
+		echo $before_widget;
+		if ( $title ){
+			echo $before_title . $title . $after_title;
+		}
+		
+?>
+		<!-- "Search by Google" plugin v.1.3 (http://wordpress.org/extend/plugins/search-google/) -->
+		<form method="get" id="tsf" action="http://www.google.com/search" name="f" class="search_google_form">
+			<fieldset>
+				<input type="text" name="pseudoq" class="pseudoq" title="Search by Google" value="" />
+				<input type="hidden" name="pseudosite" class="pseudosite" value="site:<?php echo $site_search; ?>" />
+				<input type="text" name="q" class="searchgoogle" title="Search by Google" value="site:<?php echo $site_search; ?> " />
+				<input type="submit" name="btnG" value="<?php echo $submit_text; ?>" />
+			</fieldset>
+		</form>
+<?php
+		echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$new_instance = wp_parse_args( (array) $new_instance, array( 'title' => '', 'submit_text' => __('Search Google'), 'site_search' => get_bloginfo('home') ) );
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['submit_text'] = strip_tags($new_instance['submit_text']);
+		$instance['site_search'] = strip_tags($new_instance['site_search']);
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'submit_text' => 'Google search', 'site_search' => get_bloginfo('home') ) );
+		$title = strip_tags($instance['title']);
+		$submit_text = strip_tags($instance['submit_text']);
+		$site_search = strip_tags($instance['site_search']);
+		
+?>
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id('submit_text'); ?>"><?php _e('Submit button text:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('submit_text'); ?>" name="<?php echo $this->get_field_name('submit_text'); ?>" type="text" value="<?php echo esc_attr($submit_text); ?>" />
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id('site_search'); ?>"><?php _e('Search on site:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('site_search'); ?>" name="<?php echo $this->get_field_name('site_search'); ?>" type="text" value="<?php echo esc_attr($site_search); ?>" />
+			</p>
+			
+			
+<?php
+	}
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("WP_Widget_Search_Google");'));
+
+function search_google_scripts_styles_init() {
+	if (!is_admin()) {
+		//wp_enqueue_script('jquery');
+		wp_enqueue_script( 'search-google-script', plugins_url( '/js/search-google.js', __FILE__ ), array('jquery'), '1.3' );
+		wp_enqueue_style( 'search-google-style', plugins_url( '/css/search-google.css', __FILE__ ), false, '1.3', 'all' );
+	}
+}
+add_action('init', 'search_google_scripts_styles_init');
